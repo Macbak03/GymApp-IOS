659f338051ef50903d0aeec3d88574fec8783b27
Maciej Bąk <maciejbak2003@gmail.com>
2024-11-28 17:22:41 +0100

    fixed removing exercise index out of range error, everything is changed to view model

---
+++ LiftHub/ViewModel/Home/HomeViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  HomeViewModel.swift
+//  Lift-Hub
+//
+//  Created by Maciej "wielki" Bąk on 28/11/2024.
+//
+
+import Foundation
---
+++ LiftHub/ViewModel/Workout/NoPlanWorkoutSetViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  NoPlanWorkoutSetViewModel.swift
+//  Lift-Hub
+//
+//  Created by Maciej "wielki" Bąk on 23/11/2024.
+//
+
+import Foundation
